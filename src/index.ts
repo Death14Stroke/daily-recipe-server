6f39e35be3d1d010e04850d939e255e894939632
DeaDshot14 <mistry14darren@gmail.com>
2021-04-07 20:09:58 +0530

    fixed duplicate recipe ids

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import admin from 'firebase-admin';
 import categoryRoutes from './routes/categoryRoutes';
 import recipeRoutes from './routes/recipeRoutes';
 import ingredientRoutes from './routes/ingredientRoutes';
 import requireAuth from './middlewares/requireAuth';
+
+admin.initializeApp({
+	credential: admin.credential.cert(require('../serviceAccountKey.json'))
+});
 
 const app = express();
 app.use(express.json());
@@ -11,9 +16,6 @@ app.use(requireAuth);
 app.use(categoryRoutes);
 app.use(recipeRoutes);
 app.use(ingredientRoutes);
-
-// for debugging only
-app.disable('etag');
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
--- src/middlewares/requireAuth.ts
+++ src/middlewares/requireAuth.ts
@@ -4,7 +4,7 @@ import admin from 'firebase-admin';
 export default async (req: Request, res: Response, next: NextFunction) => {
 	const { authorization } = req.headers;
 
-	if (!authorization)
+    if (!authorization)
 		return res.status(401).send({ error: 'You must be logged in' });
 
 	const token = authorization.replace('Bearer ', '');
@@ -12,7 +12,7 @@ export default async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		const { uid } = await admin.auth().verifyIdToken(token);
 		req.uid = uid;
-		next();
+        next();
 	} catch (err) {
 		return res.status(401).send({ error: 'Invalid token' });
 	}
